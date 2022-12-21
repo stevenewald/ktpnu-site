10b9b4f3ba0402ddafb826eb0573d971cb223be7
stevenewald <stevenewald6@gmail.com>
2022-12-21 14:05:32 -0400

    Auth

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Hero from './hero';
+import WhatIsKTP from './WhatIsKTP';
+import Header from './Header';
+//import firebase from 'firebase';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MemberPage from './MemberPage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={
+          <div>
+            <Header/>
+            <Hero />
+            <WhatIsKTP />
+          </div>
+        }>
+        </Route>
+        <Route path="/member" element={
+          <MemberPage />
+        }>
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
