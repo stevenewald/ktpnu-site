ae062e64a345248e14228d195237d5f38d3cb08d
stevenewald <stevenewald6@gmail.com>
2023-09-27 01:34:36 +0100

    Added simon to teams page

--- src/Components/Landing/Team.tsx
+++ src/Components/Landing/Team.tsx
@@ -5,6 +5,7 @@ import StevePic from "@images/Exec/steve.jpeg";
 import AlexisPic from "@images/Exec/alexis.jpeg";
 import DamienPic from "@images/Exec/damien.jpeg";
 import DaphnePic from "@images/Exec/daphne.jpeg";
+import SimonPic from "@images/Exec/simon.jpeg";
 
 const people: { name: string; role: string; imageUrl: any }[] = [
   {
@@ -42,6 +43,11 @@ const people: { name: string; role: string; imageUrl: any }[] = [
     role: "VP of Marketing",
     imageUrl: DaphnePic,
   },
+  {
+    name: "Simon Hedin",
+    role: "VP of Recruitment",
+    imageUrl: SimonPic,
+  }
 ];
 
 function Team() {
@@ -84,31 +90,9 @@ function Team() {
           {/* Laptop/tablet team layout */}
           <ul
             role="list"
-            className="hidden sm:grid mx-auto gap-x-4 gap-y-8 grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 md:gap-x-6 sm:max-w-2xl lg:max-w-3xl lg:gap-y-12"
-          >
-            {people.slice(0, 3).map((person) => (
-              <li>
-                <div className="space-y-4">
-                  <img
-                    className="mx-auto h-20 w-20 rounded-full lg:h-24 lg:w-24"
-                    src={person.imageUrl}
-                    alt=""
-                  />
-                  <div className="space-y-2">
-                    <div className="text-center lg:whitespace-nowrap text-xs font-medium lg:text-sm">
-                      <h3>{person.name}</h3>
-                      <p className="text-indigo-600">{person.role}</p>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-          <ul
-            role="list"
             className="hidden sm:grid mx-auto gap-x-4 gap-y-8 grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 md:gap-x-6 sm:max-w-2xl lg:max-w-3xl lg:gap-y-12"
           >
-            {people.slice(3, 7).map((person) => (
+            {people.map((person) => (
               <li>
                 <div className="space-y-4">
                   <img
