7769ca9bfe5fa4e2c77cf2b19af21fd6e782c846
carolineguerraa <carolinejjguerra@gmail.com>
2025-01-05 00:42:47 -0500

    changed visibility for rush application on landing page

--- src/Components/Landing/Hero.tsx
+++ src/Components/Landing/Hero.tsx
@@ -194,7 +194,7 @@ export default function Hero(props: { maintenance: boolean }) {
                     <a
                       href="https://rush.ktpnu.com"
                       target="_blank"
-                      className="hidden rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                       Rush Application
                     </a>
